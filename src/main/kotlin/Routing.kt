47c8006778c75edde51bf27e462c504a21f38e67
ThiagoSGomes-Dev <thiagodos.10@gmail.com>
2026-01-03 15:11:20 -0300

    [feat] cria uma base rota

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -4,41 +4,50 @@ import br.com.ktor.model.CurrencyTypeResultResponse
 import br.com.ktor.model.ExchangeRateResponse
 import br.com.ktor.model.commonCurrencies
 import br.com.ktor.model.commonExchangeRates
+import br.com.ktor.model.orUnknown
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import br.com.ktor.model.orUnknown
 
 fun Application.configureRouting() {
     routing {
-        // tipos de moedas (nome, acroimo, símbolo, imagem da bandeira do país)
-        get("/currency-types/v1/metadata") {
-            call.respond(
-                CurrencyTypeResultResponse(
-                    values = commonCurrencies
-                )
-            )
-        }
 
-        // conversão de valores, moeda atual, taxa de conversão entre essas moedas
-        get("/exchange-rate/{from}/{to}") {
-            val from = call.parameters["from"]?.uppercase() ?: return@get call.respondText (
-                status = HttpStatusCode.BadRequest,
-                text = "Não foi possível obter o acrônimo da moeda atual."
-            )
-            val to = call.parameters["to"]?.uppercase() ?: return@get call.respondText (
-                status = HttpStatusCode.BadRequest,
-                text = "Não foi possível obter o acrônimo da moeda alvo."
-            )
+        // rota base
+        route("/currency") {
 
-            call.respond(
-                ExchangeRateResponse(
-                    from = from.orUnknown(),
-                    to = to.orUnknown(),
-                    exchangeRate = commonExchangeRates[from]?.get(to) ?: 0.0
+            // tipos de moedas (nome, acrônimo, símbolo, bandeira)
+            get("/currency-types/v1/metadata") {
+                call.respond(
+                    CurrencyTypeResultResponse(
+                        values = commonCurrencies
+                    )
                 )
-            )
+            }
+
+            // taxa de conversão entre moedas
+            get("/exchange-rate/{from}/{to}") {
+
+                val from = call.parameters["from"]?.uppercase()
+                    ?: return@get call.respondText(
+                        text = "Não foi possível obter o acrônimo da moeda atual.",
+                        status = HttpStatusCode.BadRequest
+                    )
+
+                val to = call.parameters["to"]?.uppercase()
+                    ?: return@get call.respondText(
+                        text = "Não foi possível obter o acrônimo da moeda alvo.",
+                        status = HttpStatusCode.BadRequest
+                    )
+
+                call.respond(
+                    ExchangeRateResponse(
+                        from = from.orUnknown(),
+                        to = to.orUnknown(),
+                        exchangeRate = commonExchangeRates[from]?.get(to) ?: 0.0
+                    )
+                )
+            }
         }
     }
 }
